5892211f46c4c81c9c925e081fe0a7954c7c6fbe
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2025-02-24 11:50:30 +0100

    fix: add production Sequelize config

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -8,4 +8,27 @@ module.exports = {
     port: process.env.DB_PORT,
     logging: true,
   },
+  production: {
+    dialect: 'postgres',
+    host: process.env.DB_HOSTNAME,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    logging: false,
+    pool: {
+      maxConnections: Number.MAX_SAFE_INTEGER,
+      maxIdleTime: 30000,
+      max: 20,
+      min: 0,
+      idle: 20000,
+      acquire: 20000,
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
 };
